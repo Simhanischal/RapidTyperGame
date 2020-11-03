db6c44362d2b275e6fc063e4354202433e57a36b
Simha S <nischal.simha.s@accenture.com>
2020-11-03 23:37:21 +0530

    feat: added difficulty levels, high scores with difficulty levels and how to play section with difficulty level

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 function start(){
     openModal();
     scoresModal.style.display = 'none';
+    howToModal.style.display = 'none';
     startModal.style.display = 'block';
 }
 
 function openScores(){
-    getHighScores(); //show the high scores
+    getHighScores('easy'); //show the high scores
+    easyScoresButton.style.borderBottom = '2px solid #ff8000';
+    mediumScoresButton.style.borderBottom = 'none';
+    hardScoresButton.style.borderBottom = 'none';
     openModal();
     startModal.style.display = 'none';
+    howToModal.style.display = 'none';
     scoresModal.style.display = 'block';
 }
 
+function openHowToPlay(){
+    getHowtoPlay('easy'); //show the how to play section
+    easyHowToButton.style.borderBottom = '2px solid #ff8000';
+    mediumHowToButton.style.borderBottom = 'none';
+    hardHowToButton.style.borderBottom = 'none';
+    openModal();
+    startModal.style.display = 'none';
+    scoresModal.style.display = 'none';
+    howToModal.style.display = 'block';
+}
+
 function play(){
+    //let selectedDifficulty = difficulty.value;
+    modal.style.display = 'none'; //close the start modal
     userInput.value = ''; //clearing the input if any from previous session
     bgVideo.style.display = 'block'; //make the background video visible
     startArea.style.display = 'none'; //make the start area(start button and high scores button) invisible
     resetArea.style.display = 'block'; //make the reset area(reset button and exit button) visible
-    playArea.style.display = 'block'; 
+    playArea.style.display = 'block';
+    document.querySelector('#playDifficulty').innerHTML = difficulty.value.toUpperCase(); //to display in the play area the difficulty level selected 
     time = 5; //set the time to 5s
     timer(); //start the timer
     generateRandomWord(); //generate a random word
@@ -25,7 +44,7 @@ function play(){
 }
 
 function stop(){
-    addScoreToStorage(score.innerHTML); //add the score to local storage
+    addScoreToStorage(score.innerHTML,difficulty.value); //add the score to local storage
     time = 0; // reset the time
     bgVideo.style.display = 'none'; //disable the video
     startArea.style.display = 'block'; //enable the start area
@@ -51,23 +70,38 @@ function generateRandomWord(){
     document.querySelector('.word').innerHTML = words[random]; //display a random word from the list
 }
 
-function addScoreToStorage(currentScore){
-    if(localStorage.getItem('scores') === null) //this is for a first time user to initialze the scores array
-        localStorage.setItem('scores','[]');
-    let scores = JSON.parse(localStorage.getItem('scores'));
+function addScoreToStorage(currentScore,difficulty){
+    if(localStorage.getItem(`scores_${difficulty}`) === null) //this is for a first time user to initialze the scores array
+        localStorage.setItem(`scores_${difficulty}`,'[]');
+    let scores = JSON.parse(localStorage.getItem(`scores_${difficulty}`));
     scores.push(currentScore); //add the current score to the scores array
-    localStorage.setItem('scores',JSON.stringify(scores)); //add the score to local storage
+    localStorage.setItem(`scores_${difficulty}`,JSON.stringify(scores)); //add the score to local storage
 }
 
-function getHighScores(){
-    if(localStorage.getItem('scores') === null)
-        localStorage.setItem('scores',JSON.stringify([0,0,0])); //initialize an array with 3 zeros for 3 high scores for first time user
-    let scores = JSON.parse(localStorage.getItem('scores'));
+function getHighScores(difficulty){
+    if(localStorage.getItem(`scores_${difficulty}`) === null)
+        localStorage.setItem(`scores_${difficulty}`,JSON.stringify([0,0,0])); //initialize an array with 3 zeros for 3 high scores for first time user
+    let scores = JSON.parse(localStorage.getItem(`scores_${difficulty}`));
     scores.sort((a,b)=>b-a); //sort the scores array in descending order
     //add the top 3 scores in the array in high scores area
     document.querySelector('#first-high-score').innerHTML = scores[0];
     document.querySelector('#second-high-score').innerHTML = scores[1];
     document.querySelector('#third-high-score').innerHTML = scores[2];
+}
+
+function getHowtoPlay(difficulty){
+    let increment;
+    if(difficulty === 'medium'){
+        increment = '+3';
+    }
+    else if(difficulty === 'hard'){
+        increment = '+1';
+    }
+    else{
+        increment = '+5';
+    }
+    document.querySelector('#how-to-score').innerHTML = increment;
+    document.querySelector('#how-to-time').innerHTML = increment;
 }
 
 function timerInterval(){
@@ -90,9 +124,21 @@ function checkWord(event){
     //if the entered word is equal to displayed word 
     if(word.innerHTML === event.target.value.toLowerCase()){
         event.target.value = '';
-        score.innerHTML = parseInt(score.innerHTML) + 3; //increment the score by 3
-        timeLeft.innerHTML = parseInt(timeLeft.innerHTML) + 3; //increment the time by 3s
-        time += 3;
+        if(difficulty.value === 'easy'){
+            score.innerHTML = parseInt(score.innerHTML) + 5; //increment the score by 5
+            timeLeft.innerHTML = parseInt(timeLeft.innerHTML) + 5; //increment the time by 5s
+            time += 5;
+        }
+        else if(difficulty.value === 'medium'){
+            score.innerHTML = parseInt(score.innerHTML) + 3; //increment the score by 3
+            timeLeft.innerHTML = parseInt(timeLeft.innerHTML) + 3; //increment the time by 3s
+            time += 3;
+        }
+        else{
+            score.innerHTML = parseInt(score.innerHTML) + 1; //increment the score by 1
+            timeLeft.innerHTML = parseInt(timeLeft.innerHTML) + 1; //increment the time by 1s
+            time += 1;
+        }
         generateRandomWord(); //generate a new random word
     }
 }
@@ -129,6 +175,7 @@ let startButton = document.querySelector('.start-button');
 let resetButton = document.querySelector('.reset-button');
 let exitButton = document.querySelector('.exit-button');
 let trophyButton = document.querySelector('.trophy-button');
+let howToButton = document.querySelector('.how-to-button');
 let userInput = document.querySelector('#user-input');
 let word = document.querySelector('.word');
 let timeLeft = document.querySelector('#time-left');
@@ -139,8 +186,16 @@ let wellDone = document.querySelector('#well-done');
 let modal = document.querySelector('.modal');
 let startModal = document.querySelector('.start-modal');
 let scoresModal = document.querySelector('.scores-modal');
+let howToModal = document.querySelector('.how-to-modal');
 let closeModalButton = document.querySelector('.close');
 let playButton = document.querySelector('.play-button');
+let difficulty = document.querySelector('#difficulty');
+let easyScoresButton = document.querySelector('#easy-score');
+let mediumScoresButton = document.querySelector('#medium-score');
+let hardScoresButton = document.querySelector('#hard-score');
+let easyHowToButton = document.querySelector('#easy-how-to');
+let mediumHowToButton = document.querySelector('#medium-how-to');
+let hardHowToButton = document.querySelector('#hard-how-to');
 
 startButton.addEventListener("click",start);
 playButton.addEventListener("click",play);
@@ -148,5 +203,42 @@ resetButton.addEventListener("click",reset);
 exitButton.addEventListener("click",exit);
 userInput.addEventListener("input",checkWord);
 trophyButton.addEventListener("click",openScores);
+howToButton.addEventListener("click",openHowToPlay);
+easyScoresButton.addEventListener("click",()=>{
+    getHighScores('easy');
+    easyScoresButton.style.borderBottom = '2px solid #ff8000';
+    mediumScoresButton.style.borderBottom = 'none';
+    hardScoresButton.style.borderBottom = 'none';
+});
+mediumScoresButton.addEventListener("click",()=>{
+    getHighScores('medium');
+    mediumScoresButton.style.borderBottom = '2px solid #ff8000';
+    easyScoresButton.style.borderBottom = 'none';
+    hardScoresButton.style.borderBottom = 'none';
+});
+hardScoresButton.addEventListener("click",()=>{
+    getHighScores('hard');
+    hardScoresButton.style.borderBottom = '2px solid #ff8000';
+    easyScoresButton.style.borderBottom = 'none';
+    mediumScoresButton.style.borderBottom = 'none';
+});
+easyHowToButton.addEventListener("click",()=>{
+    getHowtoPlay('easy');
+    easyHowToButton.style.borderBottom = '2px solid #ff8000';
+    mediumHowToButton.style.borderBottom = 'none';
+    hardHowToButton.style.borderBottom = 'none';
+});
+mediumHowToButton.addEventListener("click",()=>{
+    getHowtoPlay('medium');
+    mediumHowToButton.style.borderBottom = '2px solid #ff8000';
+    easyHowToButton.style.borderBottom = 'none';
+    hardHowToButton.style.borderBottom = 'none';
+});
+hardHowToButton.addEventListener("click",()=>{
+    getHowtoPlay('hard');
+    hardHowToButton.style.borderBottom = '2px solid #ff8000';
+    easyHowToButton.style.borderBottom = 'none';
+    mediumHowToButton.style.borderBottom = 'none';
+});
 closeModalButton.addEventListener("click",closeModal);
 window.addEventListener("click",closeModal);
